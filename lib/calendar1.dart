1ec4ba5f732a92edb742214423b5f76223e4489c
romajc84 <joeyroman84@gmail.com>
2022-09-09 09:35:59 -0400

    reset times function working.

--- lib/calendar1.dart
+++ lib/calendar1.dart
@@ -10,172 +10,211 @@ class Calendar1 extends StatefulWidget {
 }
 
 class _Calendar1State extends State<Calendar1> {
-  DateTime selectedDate = DateTime.now();
-  int currentDateSelectedIndex = 0;
-  ScrollController scrollController = ScrollController();
+  int _selectedIndex = 0;
+  final ScrollController _scrollController = ScrollController();
 
-  DateTime intialTime = DateTime.now();
-  DateTime? time;
+  DateTime _initialTime = DateTime.now();
+  DateTime? _time;
+  final int _daysInCalendar = 60;
 
-  List<String> listOfMonths = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec"
+  final List<String> _listOfDays = [
+    "MON",
+    "TUE",
+    "WED",
+    "THU",
+    "FRI",
+    "SAT",
+    "SUN"
   ];
 
-  List<String> listOfDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  List<String> listOfTimes = List<String>.filled(60, "", growable: true);
 
-  List<String> listOfTimes = List<String>.filled(60, "");
+  late DateTime _selectedDate;
+
+  @override
+  void initState() {
+    super.initState();
+    _resetSelectedDate();
+  }
+
+  void _resetSelectedDate() {
+    _selectedDate = DateTime.now();
+    _selectedIndex = 0;
+  }
+
+  void _scrollToIndex(index) {
+    _scrollController.animateTo(60.0 * index,
+        duration: const Duration(milliseconds: 500), curve: Curves.easeIn);
+  }
+
+  void _resetTimesIndex(DateTime oldTime) {
+    final today = DateTime.now();
+    int daysBetween(DateTime from, DateTime to) {
+      from = DateTime(from.year, from.month, from.day);
+      to = DateTime(to.year, to.month, to.day);
+      return (to.difference(from).inHours / 24).round();
+    }
+
+    int dayDifference = daysBetween(oldTime, today);
+    if (dayDifference > 0) {
+      for (int i = 0; i < dayDifference; i++) {
+        listOfTimes.removeAt(0);
+        listOfTimes.add("");
+      }
+    }
+    return;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          automaticallyImplyLeading: false,
-          title: const Text('My Calendar'),
-        ),
-        body: Column(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: MyTheme.primary,
+        centerTitle: true,
+        elevation: 0,
+        toolbarHeight: 42.0,
+
+        /// For testing [_resetTimesIndex] only
+
+        leading: Row(
           children: [
-            //Text Widget to Show Current Date
-            Container(
-                height: 30,
-                margin: const EdgeInsets.only(left: 10),
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  '${selectedDate.day}-${listOfMonths[selectedDate.month - 1]}, ${selectedDate.year}',
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w800,
-                      color: Colors.indigo[700]),
-                )),
-            const SizedBox(height: 10),
-
-            //ListView Widget for the Calendar
-            SizedBox(
-              height: 100,
-              child: ListView.separated(
-                separatorBuilder: (BuildContext context, int index) {
-                  return const SizedBox(width: 10);
-                },
-                itemCount: 60,
-                controller: scrollController,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (BuildContext context, int index) {
-                  return InkWell(
-                    onTap: () {
-                      setState(() {
-                        currentDateSelectedIndex = index;
-                        selectedDate =
-                            DateTime.now().add(Duration(days: index));
-                      });
-                      showCupertinoModalPopup<void>(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return _buildBottomPicker(timePicker());
-                          });
-                    },
-                    child: Container(
-                      height: 80,
-                      width: 60,
-                      alignment: Alignment.center,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: const [
-                            BoxShadow(
-                                color: Colors.grey,
-                                offset: Offset(3, 3),
-                                blurRadius: 5)
-                          ],
-                          color: currentDateSelectedIndex == index
-                              ? Colors.black
-                              : Colors.white),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            listOfMonths[DateTime.now()
-                                        .add(Duration(days: index))
-                                        .month -
-                                    1]
-                                .toString(),
-                            style: TextStyle(
-                                fontSize: 16,
-                                color: currentDateSelectedIndex == index
-                                    ? Colors.white
-                                    : Colors.grey),
-                          ),
-                          const SizedBox(height: 5),
-                          Text(
-                            DateTime.now()
-                                .add(Duration(days: index))
-                                .day
-                                .toString(),
-                            style: TextStyle(
-                                fontSize: 22,
-                                fontWeight: FontWeight.w700,
-                                color: currentDateSelectedIndex == index
-                                    ? Colors.white
-                                    : Colors.grey),
-                          ),
-                          const SizedBox(height: 5),
-                          Text(
-                            listOfDays[DateTime.now()
-                                        .add(Duration(days: index))
-                                        .weekday -
-                                    1]
-                                .toString(),
-                            // listOfDays[index][1],
-                            style: TextStyle(
-                                fontSize: 16,
-                                color: currentDateSelectedIndex == index
-                                    ? Colors.white
-                                    : Colors.grey),
-                          ),
-                          const SizedBox(height: 5),
-                          Text(
-                            // listOfDays[DateTime.now()
-                            //         .add(Duration(days: index))
-                            //         .weekday -
-                            //     1][1],
-
-                            listOfTimes[index],
-                            style: TextStyle(
-                                fontSize: 16,
-                                color: currentDateSelectedIndex == index
-                                    ? Colors.white
-                                    : Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ),
-                  );
-                },
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  _resetTimesIndex(_initialTime);
+                });
+              },
+              icon: const Icon(
+                Icons.arrow_back,
               ),
             ),
           ],
         ),
+
+        /// End of test
+
+        title: Text(
+          DateFormat.yMMMMd('en_US').format(_selectedDate).toString(),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  _resetSelectedDate();
+                  _scrollToIndex(_selectedIndex);
+                });
+              },
+              icon: const Icon(Icons.refresh)),
+        ],
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 10),
+
+          //ListView Widget for the Calendar
+          SizedBox(
+            height: 75,
+            child: ListView.builder(
+              itemCount: _daysInCalendar,
+              controller: _scrollController,
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                  onTap: () {
+                    setState(() {
+                      _selectedIndex = index;
+                      _selectedDate = DateTime.now().add(Duration(days: index));
+                      _scrollToIndex(index);
+                    });
+                    showCupertinoModalPopup<void>(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return _buildBottomPicker(timePicker());
+                        });
+                  },
+                  child: Container(
+                    width: 60,
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(12.0),
+                        color: _selectedIndex == index
+                            ? MyTheme.primary
+                            : Colors.transparent),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 3),
+                        Text(
+                          _listOfDays[DateTime.now()
+                                      .add(Duration(days: index))
+                                      .weekday -
+                                  1]
+                              .toString(),
+                          style: _selectedIndex == index
+                              ? const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                )
+                              : const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                        ),
+                        if (_selectedIndex == index) ...[
+                          const Padding(
+                            padding: EdgeInsets.fromLTRB(5, 1, 5, 9),
+                            child: Divider(
+                              color: Colors.white,
+                              height: 0,
+                              thickness: 1,
+                            ),
+                          ),
+                        ] else
+                          const SizedBox(),
+                        Text(
+                          DateTime.now()
+                              .add(Duration(days: index))
+                              .day
+                              .toString(),
+                          style: _selectedIndex == index
+                              ? TextStyle(
+                                  color: Colors.redAccent[100],
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                  height: 0.8,
+                                )
+                              : TextStyle(
+                                  color: MyTheme.primary,
+                                  fontSize: 32,
+                                ),
+                        ),
+                        Text(
+                          listOfTimes[index],
+                          style: _selectedIndex == index
+                              ? const TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 10,
+                                )
+                              : const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 10,
+                                ),
+                        ),
+                        const SizedBox(height: 3),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
-  }
-
-  _addTimeToIndex() {
-    if (time == null) {
-      listOfTimes[currentDateSelectedIndex] = "";
-    } else {
-      listOfTimes[currentDateSelectedIndex] =
-          DateFormat.Hm().format(time!).toString();
-    }
   }
 
   Widget _buildBottomPicker(Widget picker) {
@@ -205,14 +244,24 @@ class _Calendar1State extends State<Calendar1> {
       mode: CupertinoDatePickerMode.time,
       use24hFormat: true,
       minuteInterval: 1,
-      initialDateTime: intialTime,
+      initialDateTime: _initialTime,
       onDateTimeChanged: (DateTime changedTime) {
         setState(() {
-          intialTime = changedTime;
-          time = changedTime;
-          _addTimeToIndex();
+          _initialTime = changedTime;
+          _time = changedTime;
+          if (_time == null) {
+            listOfTimes[_selectedIndex] = "";
+          } else {
+            listOfTimes[_selectedIndex] =
+                DateFormat.Hm().format(_time!).toString();
+          }
         });
       },
     );
   }
+}
+
+class MyTheme {
+  static Color primary = const Color.fromRGBO(17, 46, 81, 1);
+  static Color secondary = const Color.fromRGBO(0, 113, 188, 1);
 }
